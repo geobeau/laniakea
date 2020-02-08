b70c3ac51140a3d22fe542a23142b827925e783c
Geoffrey Beausire <g.beausire@criteo.com>
2020-02-08 19:23:49 +0100

    Implement flush queue

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -1,16 +1,18 @@
 package memtable
 
 import (
-	"github.com/geobeau/laniakea/memtable/skiplist"
+	"container/list"
 	"sync"
+
+	"github.com/geobeau/laniakea/memtable/skiplist"
 )
 
 // RollingMemtable contains the dataset in memory
 // It is flushed to disk at regular interval
 type RollingMemtable struct {
 	mutex       *sync.RWMutex
-	activeTable memtable
-	flushQueue  []memtable
+	activeTable *memtable
+	flushQueue  *list.List
 }
 
 // NewRollingMemtable create a new rollingMemtable
@@ -18,15 +20,35 @@ func NewRollingMemtable() RollingMemtable {
 	return RollingMemtable{
 		mutex:       &sync.RWMutex{},
 		activeTable: newMemtable(),
+		flushQueue:  list.New(),
 	}
 }
 
+func (m *RollingMemtable) flushActive() {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	m.flushQueue.PushFront(m.activeTable)
+	m.activeTable = newMemtable()
+}
+
 // Get a key from the memtables
-func (m *RollingMemtable) Get(key string) Element {
+func (m *RollingMemtable) Get(key string) (Element, bool) {
 	m.mutex.RLock()
 	defer m.mutex.RUnlock()
 
-	return m.activeTable.get(key)
+	elem, found := m.activeTable.get(key)
+	if found {
+		return elem, found
+	}
+	for e := m.flushQueue.Front(); e != nil; e = e.Next() {
+		table := e.Value.(memtable)
+		table.get(key)
+		if found {
+			return elem, found
+		}
+	}
+	return Element{}, false
 }
 
 // Set a key to the active memtable
@@ -54,13 +76,16 @@ type Element struct {
 	Value     []byte
 }
 
-func newMemtable() memtable {
-	return memtable{skiplist.New()}
+func newMemtable() *memtable {
+	return &memtable{skiplist.New()}
 }
 
-func (m *memtable) get(key string) Element {
+func (m *memtable) get(key string) (Element, bool) {
 	elem := m.skiplist.Get(key)
-	return elem.Value().(Element)
+	if elem == nil {
+		return Element{}, false
+	}
+	return elem.Value().(Element), true
 }
 
 func (m *memtable) set(key string, value Element) {
