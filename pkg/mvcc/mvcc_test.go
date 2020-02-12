38120fd4198b84607e0ec8b29766c6a2c0f93ff4
Geoffrey Beausire <g.beausire@criteo.com>
2020-02-12 21:25:21 +0100

    Add testing for the elemstack

--- pkg/mvcc/mvcc_test.go
+++ pkg/mvcc/mvcc_test.go
@@ -1,6 +1,7 @@
 package mvcc
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -31,5 +32,79 @@ func TestTombstoneReturnTombstone(t *testing.T) {
 	}
 	if elem.Tombstone == false {
 		t.Error("Tombstone doesn't have tombstone flag")
+	}
+}
+
+func TestNewElemStackCorrectlyInitializeStack(t *testing.T) {
+	elem := NewElement("test", []byte("val"))
+	stack := NewElemStack(elem)
+	stack.mutex.Lock()
+	stack.mutex.Unlock()
+	latest := stack.GetLatest()
+	if !reflect.DeepEqual(elem, latest) {
+		t.Error("Stack is not properly initialized")
+	}
+}
+
+func TestPushIsFailingIfOlderThanLatest(t *testing.T) {
+	elem := NewElement("test", []byte("val"))
+	elem2 := NewElement("test", []byte("val"))
+	stack := NewElemStack(elem2)
+	success := stack.Push(elem)
+	if success {
+		t.Error("Push should fail is the timestamp is older than the latest")
+	}
+}
+
+func TestPushSucceedIfNewerThanLast(t *testing.T) {
+	elem := NewElement("test", []byte("val"))
+	elem2 := NewElement("test", []byte("val"))
+	stack := NewElemStack(elem)
+	success := stack.Push(elem2)
+	latest := stack.GetLatest()
+	if !success || !reflect.DeepEqual(elem2, latest) {
+		t.Error("Push should fail is the timestamp is older than the latest")
+	}
+}
+
+func TestGetWithTimestampReturnCorrectElem(t *testing.T) {
+	elem1 := NewElement("test", []byte("val"))
+	elem2 := NewElement("test", []byte("val"))
+	elem3 := NewElement("test", []byte("val"))
+	stack := NewElemStack(elem1)
+	stack.Push(elem2)
+	stack.Push(elem3)
+	elem, found := stack.GetWithTimestamp(elem2.Timestamp)
+
+	if !found || !reflect.DeepEqual(elem2, elem) {
+		t.Error("GetWithTimestamp return wrong element")
+	}
+}
+
+func TestGetWithTimestampReturnFalseIfOlder(t *testing.T) {
+	elem1 := NewElement("test", []byte("val"))
+	elem2 := NewElement("test", []byte("val"))
+	elem3 := NewElement("test", []byte("val"))
+	stack := NewElemStack(elem1)
+	stack.Push(elem2)
+	stack.Push(elem3)
+	ts := elem1.Timestamp
+	ts.wall -= 100
+	_, found := stack.GetWithTimestamp(ts)
+
+	if found {
+		t.Error("GetWithTimestamp should return false if requested elem is older than oldest")
+	}
+}
+
+func TestGetWithTimestampReturnFalseIfEmpty(t *testing.T) {
+	elem1 := NewElement("test", []byte("val"))
+	stack := NewElemStack(elem1)
+	stack.stack = []Element{}
+	ts := elem1.Timestamp
+	_, found := stack.GetWithTimestamp(ts)
+
+	if found {
+		t.Error("GetWithTimestamp should return false if stack is empty")
 	}
 }
